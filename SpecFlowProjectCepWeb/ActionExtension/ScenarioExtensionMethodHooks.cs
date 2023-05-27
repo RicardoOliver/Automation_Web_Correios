56bde05597296e0564a2954f92db76226e7f7ccb
RicardoOliver <r.c.d.1985@hotmail.com>
2023-05-27 11:03:59 -0300

    first commit

---
+++ SpecFlowProjectCepWeb/ActionExtension/ScenarioExtensionMethodHooks.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Bindings;
+
+namespace SpecFlowProjectCepWeb.ActionExtension
+{
+    public static class ScenarioExtensionMethodHooks
+    {
+        // Cria um nó de cenário com base no tipo de definição de passo (Given/Then/When)
+        private static ExtentTest CreateScenario(ExtentTest extent, StepDefinitionType stepDefinitionType)
+        {
+            var scenarioStepContext = ScenarioStepContext.Current.StepInfo.Text;
+
+            switch (stepDefinitionType)
+            {
+                case StepDefinitionType.Given:
+                    return extent.CreateNode<Given>(scenarioStepContext);
+
+                case StepDefinitionType.Then:
+                    return extent.CreateNode<Then>(scenarioStepContext);
+
+                case StepDefinitionType.When:
+                    return extent.CreateNode<When>(scenarioStepContext);
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stepDefinitionType), stepDefinitionType, null);
+            }
+        }
+
+        // Cria um nó de cenário para falha ou erro com base no tipo de definição de passo (Given/Then/When)
+        private static void CreateScenarioFailOrError(ExtentTest extent, StepDefinitionType stepDefinitionType)
+        {
+            var error = ScenarioContext.Current.TestError;
+
+            if (error.InnerException == null)
+            {
+                CreateScenario(extent, stepDefinitionType).Error(error.Message);
+            }
+            else
+            {
+                CreateScenario(extent, stepDefinitionType).Fail(error.InnerException);
+            }
+        }
+
+        // Método de extensão para criar um nó de cenário para uma definição de passo Given
+        public static void StepDefinitionGiven(this ExtentTest extent)
+        {
+            if (ScenarioContext.Current.TestError == null)
+                CreateScenario(extent, StepDefinitionType.Given);
+            else
+                CreateScenarioFailOrError(extent, StepDefinitionType.Given);
+        }
+
+        // Método de extensão para criar um nó de cenário para uma definição de passo When
+        public static void StepDefinitionWhen(this ExtentTest extent)
+        {
+            if (ScenarioContext.Current.TestError == null)
+                CreateScenario(extent, StepDefinitionType.When);
+            else
+                CreateScenarioFailOrError(extent, StepDefinitionType.When);
+        }
+
+        // Método de extensão para criar um nó de cenário para uma definição de passo Then
+        public static void StepDefinitionThen(this ExtentTest extent)
+        {
+            if (ScenarioContext.Current.TestError == null)
+                CreateScenario(extent, StepDefinitionType.Then);
+            else
+                CreateScenarioFailOrError(extent, StepDefinitionType.Then);
+        }
+    }
+}
---
+++ SpecFlowProjectCepWeb/Common/Hooks.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using SpecFlowProjectCepWeb.ActionExtension;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Bindings;
+
+namespace SpecFlowProjectCepWeb.Common
+{
+    [Binding]
+    public class Hooks
+    {
+        private static ExtentTest _feature;
+        private static ExtentTest _scenario;
+        private static ExtentReports _extent;
+        private static readonly string PathReport = $"{AppDomain.CurrentDomain.BaseDirectory.Replace("\\bin\\Debug", "")}ExtentReportsBuscaCep.html";
+
+        [BeforeTestRun]
+        public static void ConfigureReport()
+        {
+            // Configura o reporter HTML para o ExtentReports
+            var reporter = new ExtentHtmlReporter(PathReport);
+
+            // Inicializa uma nova instância do ExtentReports
+            _extent = new ExtentReports();
+
+            // Anexa o reporter ao ExtentReports
+            _extent.AttachReporter(reporter);
+        }
+
+        [BeforeFeature]
+        public static void CreateFeature()
+        {
+            // Cria um novo teste para a feature atual
+            _feature = _extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        public static void CreateScenario()
+        {
+            // Cria um novo nó de teste para o cenário atual dentro da feature
+            _scenario = _feature.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+
+        [AfterStep]
+        public static void InsertReportingSteps()
+        {
+            // Insere os passos de relatório com base no tipo de definição de passo (Given/Then/When)
+            switch (ScenarioStepContext.Current.StepInfo.StepDefinitionType)
+            {
+                case StepDefinitionType.Given:
+                    _scenario.StepDefinitionGiven();
+                    break;
+
+                case StepDefinitionType.Then:
+                    _scenario.StepDefinitionThen();
+                    break;
+
+                case StepDefinitionType.When:
+                    _scenario.StepDefinitionWhen();
+                    break;
+            }
+        }
+
+        [AfterTestRun]
+        public static void FlushExtent()
+        {
+            // Finaliza o ExtentReports e gera o relatório
+            _extent.Flush();
+
+            // Abre o relatório no navegador padrão
+            System.Diagnostics.Process.Start(PathReport);
+        }
+    }
+}
+
